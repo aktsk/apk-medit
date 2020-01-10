2c498548ccbb1c1b5cb852e9fc2a9b8d576fe715
Yusuke Tanaka <y.tanaka@aktsk.jp>
2020-01-10 17:15:51 +0900

    Add go-prompt: ps is not working on andorid device

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/mitchellh/go-ps"
+
+	prompt "github.com/c-bata/go-prompt"
 	sys "golang.org/x/sys/unix"
 )
 
-func main() {
-	pid := os.Args[1]
+func plist() {
+	processes, err := ps.Processes()
+	if err != nil {
+		return
+	}
+
+	for i, p := range processes {
+		fmt.Printf("%d : %+v\n", i, p)
+	}
+}
+
+func attach(pid string) {
 	fmt.Printf("Target PID: %s\n", pid)
 	tidinfo, err := ioutil.ReadDir("/proc/" + pid + "/task")
 	if err != nil {
@@ -37,4 +51,36 @@ func main() {
 		sys.PtraceDetach(tid)
 		fmt.Printf("Detached TID: %d\n", tid)
 	}
+}
+
+func executor(t string) {
+	if t == "ps" {
+		plist()
+	}
+
+	if strings.HasPrefix(t, "attach") {
+		slice := strings.Split(t, " ")
+		attach(slice[1])
+	}
+
+	if t == "exit" {
+		os.Exit(0)
+	}
+	return
+}
+
+func completer(t prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{
+		{Text: "ps"},
+		{Text: "attach"},
+		{Text: "exit"},
+	}
+}
+
+func main() {
+	p := prompt.New(
+		executor,
+		completer,
+	)
+	p.Run()
 }
